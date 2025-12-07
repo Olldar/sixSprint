11a4dd0f3b08a84828cadcdf98a94ac9d5af21ed
Егор Гл <egorgl@MacBook-Air-Egor.local>
2025-12-08 02:24:26 +0300

    local is worked

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+	//"sixSprint/internal/server"
+)
+
 func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Server starting on :8080")
+
+	if err := srv.HTTP.ListenAndServe(); err != nil {
+		logger.Fatalf("server failed: %v", err)
+	}
 
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+//функция распознает тип текста и вызывает соотв.
+//ToText(morse string) string или ToMorse(text string) string
+func AutoDetectAndConvert(text string) string {
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return ""
+	}
+
+	firstSimbol := text[:1]
+
+	if firstSimbol == "."|| firstSimbol == "-" {
+		return morse.ToText(text)
+	} else {
+		return morse.ToMorse(text)
+	}
+}
